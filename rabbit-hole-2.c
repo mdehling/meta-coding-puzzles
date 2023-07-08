68a72f88eafab80986163c34096704e6c7481be0
Malte Dehling <mdehling@gmail.com>
2023-07-07 21:47:08 -0700

    Rewrite rabbit hole 2 (solves all)

--- rabbit-hole-2.c
+++ rabbit-hole-2.c
@@ -37,15 +37,18 @@
 /*
  * Solution
  *
- * 1) Contract cycles.
+ * 1) Reduce graph to strongly connected components.
  *
- * The link graph has the pages as nodes and the links as (directed) edges.
- * Because we are allowed to revisit nodes, but the page view counts only
- * once, it doesn't matter where we enter and exit a link cycle.  This means
- * that we can contract any cycle to a node to which we assign as a weight the
- * number of pages in the cycle.
+ * The link graph has pages as nodes and links as (directed) edges. Because we
+ * are allowed to revisit nodes, but the page view counts only once, it
+ * doesn't matter where we enter and exit a strongly connected component (a
+ * subgraph in which every node is reachable from every other).  This means
+ * that we can consider the graph of strongly connected components to which we
+ * assign as a weight the number of vertices in each component.
  *
  * 2) Find longest path in directed acyclic graph.
+ *
+ * This is a simple depth-first search.
  */
 
 #include <stdio.h>
@@ -53,193 +56,228 @@
 #include <strings.h>
 
 
+#define MIN(x,y)    ( (x) < (y) ? (x) : (y) )
 #define MAX(x,y)    ( (x) > (y) ? (x) : (y) )
 
-typedef struct page {
-    int weight;
-    int num_links;
-    int *link;
-} page_t;
+
+typedef struct graph {
+    int num_vertices, num_edges;
+    int *weight;                    // [num_vertices]
+    int *num_edges_;                // [num_vertices]
+    int **edge;                     // [num_vertices][num_edges_[]]
+} graph_t;
 
 
-typedef struct index {
-    int num_pages;
-    page_t *page;
-    int *links;
-} index_t;
+graph_t *graph_new(int num_vertices, int num_edges, int *source, int *target) {
+    graph_t *g = malloc(sizeof *g);
 
+    g->num_vertices = num_vertices;
+    g->num_edges = num_edges;
 
-void create_index(index_t *ip, int num_pages, int *weight,
-        int num_links, int *link_src, int *link_dst)
-{
-    page_t *page = malloc(num_pages * sizeof *page);
-    int *links = malloc(num_links * sizeof *links);
+    g->weight = calloc(num_vertices, sizeof *g->weight);
+    g->num_edges_ = calloc(num_vertices, sizeof *g->num_edges_);
 
-    // initialize
-    for (int j = 0; j < num_pages; j++) {
-        page[j].weight = weight[j];
-        page[j].num_links = 0;
-    };
+    // count number of edges for each vertex
+    for (int j = 0; j < num_edges; j++)
+        g->num_edges_[source[j]-1]++;
 
-    // count the number of links on each page
-    for (int i = 0; i < num_links; i++) {
-        int src = link_src[i]-1, dst = link_dst[i]-1;
-        if (dst != src)
-            page[src].num_links++;
-    };
+    // allocate space for edges
+    g->edge = malloc(num_vertices * sizeof *g->edge);
+    g->edge[0] = malloc(num_edges * sizeof *g->edge[0]);
+    for (int i = 1; i < num_vertices; i++)
+        g->edge[i] = g->edge[i-1] + g->num_edges_[i-1];
 
-    // set up the page links pointers
-    page[0].link = links;
-    for (int j = 1; j < num_pages; j++)
-        page[j].link = page[j-1].link + page[j-1].num_links;
-
-    // fill page links
-    int *I = calloc(num_pages, sizeof *I);
-    for (int i = 0; i < num_links; i++) {
-        int src = link_src[i]-1, dst = link_dst[i]-1;
-        if (dst != src)
-            page[src].link[I[src]++] = dst;
+    // fill in the edges
+    int *I = calloc(num_vertices, sizeof *I);
+    for (int j = 0; j < num_edges; j++) {
+        int src = source[j]-1, tgt = target[j]-1;
+        g->edge[src][I[src]++] = tgt;
     };
     free(I);
 
-    // write index
-    *ip = (index_t){.num_pages = num_pages, .page = page, .links = links};
+    return g;
 }
 
 
-void print_index(index_t *ip) {
-    for (int j = 0; j < ip->num_pages; j++) {
-        // print the (possibly empty) list of links for each page
-        printf("page[%d] = { .weight = %d, .link = [", j, ip->page[j].weight);
-        for (int i = 0; i < ip->page[j].num_links; i++)
-            printf(" %d", ip->page[j].link[i]);
-        printf(" ] }\n");
-    };
+void graph_delete(graph_t *g) {
+    if (!g)
+        return;
+
+    free(g->edge[0]);
+    free(g->edge);
+    free(g->num_edges_);
+    free(g->weight);
+    free(g);
 }
 
 
-void find_circuits(index_t *ip, int *nc, int **cp) {
-    int *c = calloc(ip->num_pages, sizeof *c), n = 0;
-    int *I = malloc(ip->num_pages * sizeof *I);
-    int s[ip->num_pages], si = 0;
-
-    /*
-     * Create page -> circuit mapping
-     */
-    for (int j = 0; j < ip->num_pages; j++) {
-        if (c[j])
-            continue;
-
-        c[j] = ++n;
-        s[si++] = j;
-
-        bzero(I, ip->num_pages * sizeof *I);
-        while (si) {
-            int cpi = s[si-1];
-            page_t *cp = ip->page + cpi;
-
-            int *cpI = I + cpi;
-            if (*cpI < cp->num_links) {
-                int tpi = cp->link[(*cpI)++];
-                if (c[tpi]) {
-                    // target part of circuit
-                    if (c[tpi] == n)
-                        c[cpi] = n;
-                } else {
-                    s[si++] = tpi;
-                };
-            } else {
-                // done with current page
-                if (--si && c[cpi])
-                    c[s[si-1]] = c[cpi];
-            };
+void graph_print(graph_t *g) {
+    printf("digraph {\n");
+    for (int i = 0; i < g->num_vertices; i++) {
+        printf("  %d (w = %d)", i+1, g->weight[i]);
+        if (g->num_edges_[i] > 0) {
+            printf(" -> {", i+1);
+            for (int j = 0; j < g->num_edges_[i]; j++)
+                printf(" %d", g->edge[i][j]+1);
+            printf(" }");
         };
-    };
-    free(I);
-
-    /*
-     * Print circuits for debugging
-     */
-    /*
-    for (int k = 0; k < n; k++) {
-        printf("circuit %d:", k+1);
-        for (int j = 0; j < ip->num_pages; j++)
-            if (c[j] == k+1)
-                printf(" %d", j);
         printf("\n");
     };
-    */
-
-    *nc = n, *cp = c;
+    printf("}\n");
 }
 
 
-int maximize_weight(index_t *ip) {
-    int *w = calloc(ip->num_pages, sizeof *w), W = 0;
-    int *I = malloc(ip->num_pages * sizeof *I);
-    int s[ip->num_pages], si = 0;
+void scc_find(graph_t *g, int v, int *np,
+        int *index, int *low_link, int *on_stack,
+        int *s, int *sip,
+        int *scc, int *num_sccp)
+{
+    index[v] = *np, low_link[v] = *np, (*np)++;
+    s[(*sip)++] = v, on_stack[v] = 1;
 
-    // total weight of a page is the max of weight of targets + page weight
-    for (int j = 0; j < ip->num_pages; j++) {
-        if (w[j])
+    for (int j = 0; j < g->num_edges_[v]; j++) {
+        int w = g->edge[v][j];
+        if (!index[w]) {
+            scc_find(g, w, np, index, low_link, on_stack, s, sip, scc, num_sccp);
+            low_link[v] = MIN(low_link[v], low_link[w]);
+        } else if (on_stack[w]) {
+            low_link[v] = MIN(low_link[v], index[w]);
+        };
+    };
+
+    if (low_link[v] == index[v]) {
+        int w;
+        do {
+            w = s[--(*sip)], on_stack[w] = 0;
+            scc[w] = *num_sccp;
+        } while (w != v);
+        (*num_sccp)++;
+    };
+}
+
+
+/*
+ * Reduce graph to its strongly connected components using Tarjan's algorithm.
+ */
+graph_t *graph_scc_reduce(graph_t *g) {
+    int *scc = calloc(g->num_vertices, sizeof *scc), num_scc = 0;
+
+    //
+    // Use Tarjan's algorithm to find strongly connected components (SCC)
+    //
+    int *index = calloc(g->num_vertices, sizeof *index);
+    int *low_link = calloc(g->num_vertices, sizeof *low_link);
+    int *on_stack = calloc(g->num_vertices, sizeof *on_stack);
+
+    int n = 1;
+    int s[g->num_vertices], si = 0;
+
+    for (int i = 0; i < g->num_vertices; i++) {
+        if (index[i])
             continue;
 
-        s[si++] = j;
+        scc_find(g, i, &n, index, low_link, on_stack, s, &si, scc, &num_scc);
+    };
 
-        bzero(I, ip->num_pages * sizeof *I);
-        while (si) {
-            int cpi = s[si-1];
-            page_t *cp = ip->page + cpi;
+    free(index);
+    free(low_link);
+    free(on_stack);
 
-            int *cpI = I + cpi;
-            if (*cpI < cp->num_links) {
-                int tpi = cp->link[(*cpI)++];
-                // skip links to self
-                if (!w[tpi])
-                    s[si++] = tpi;
-            } else {
-                // done with current page
-                for (int i = 0; i < cp->num_links; i++)
-                    w[cpi] = MAX(w[cpi], w[cp->link[i]]);
-                w[cpi] += cp->weight;
-                si--;
+
+    //
+    // Generate the SCC graph from g and the results of Tarjan's algorithm
+    //
+    graph_t *sccg = malloc(sizeof *sccg);
+
+    sccg->num_vertices = num_scc;
+    sccg->num_edges = 0;
+
+    sccg->weight = calloc(num_scc, sizeof *sccg->weight);
+    sccg->num_edges_ = calloc(num_scc, sizeof *sccg->num_edges_);
+
+    // go through the original graph's edges and count those that remain valid
+    for (int i = 0; i < g->num_vertices; i++) {
+        for (int j = 0; j < g->num_edges_[i]; j++) {
+            int src = scc[i], tgt = scc[g->edge[i][j]];
+            if (src != tgt) {
+                sccg->num_edges++;
+                sccg->num_edges_[src]++;
             };
         };
+    };
 
-        W = MAX(W, w[j]);
+    // reserve space for edges
+    sccg->edge = malloc(num_scc * sizeof *sccg->edge);
+    sccg->edge[0] = malloc(sccg->num_edges * sizeof *sccg->edge[0]);
+    for (int i = 1; i < num_scc; i++)
+        sccg->edge[i] = sccg->edge[i-1] + sccg->num_edges_[i-1];
+
+    // fill in edges & update weights
+    int *I = calloc(num_scc, sizeof *I);
+    for (int i = 0; i < g->num_vertices; i++) {
+        sccg->weight[scc[i]]++;
+        for (int j = 0; j < g->num_edges_[i]; j++) {
+            int src = scc[i], tgt = scc[g->edge[i][j]];
+            if (src != tgt) {
+                sccg->edge[src][I[src]++] = tgt;
+            };
+        };
     };
     free(I);
-    free(w);
 
+    free(scc);
+    return sccg;
+}
+
+
+/*
+ * Find longest path in a directed acyclic graph by DFS
+ */
+int graph_longest_path(graph_t *g) {
+    int W = 0;
+    int *w = calloc(g->num_vertices, sizeof *w);
+
+    int r;
+    int i[g->num_vertices];
+    int j[g->num_vertices];
+
+    for (int i0 = 0; i0 < g->num_vertices; i0++) {
+        if (w[i0])
+            continue;
+
+        r = 0, i[r] = i0, j[r] = 0;
+        while (r >= 0) {
+            if (j[r] < g->num_edges_[i[r]]) {
+                int tgt = g->edge[i[r]][j[r]++];
+                if (!w[tgt]) {
+                    r++, i[r] = tgt, j[r] = 0;
+                };
+            } else {
+                // done with this vertex
+                for (j[r] = 0; j[r] < g->num_edges_[i[r]]; j[r]++) {
+                    int tgt = g->edge[i[r]][j[r]];
+                    w[i[r]] = MAX(w[i[r]], w[tgt]);
+                };
+                w[i[r]] += g->weight[i[r]];
+                r--;
+            };
+        };
+        W = MAX(W, w[i0]);
+    };
+
+    free(w);
     return W;
 }
 
 
 int getMaxVisitableWebpages(int N, int M, int *A, int *B) {
-    index_t page_index, circuit_index;
-    int *page_weight = malloc(N * sizeof *page_weight);
-    int num_circuits, *circuit;
+    graph_t *g = graph_new(N, M, A, B);
+    // graph_print(g);
 
-    printf("\n");
+    graph_t *sccg = graph_scc_reduce(g);
+    // graph_print(sccg);
 
-    for (int j = 0; j < N; j++)
-        page_weight[j] = 1;
-
-    create_index(&page_index, N, page_weight, M, A, B);
-
-    find_circuits(&page_index, &num_circuits, &circuit);
-
-    int *circuit_weight = calloc(num_circuits, sizeof *circuit_weight);
-    for (int j = 0; j < N; j++)
-        circuit_weight[circuit[j]-1] += page_weight[j];
-
-    for (int i = 0; i < M; i++)
-        A[i] = circuit[A[i]-1], B[i] = circuit[B[i]-1];
-
-    create_index(&circuit_index, num_circuits, circuit_weight, M, A, B);
-    print_index(&circuit_index);
-
-    return maximize_weight(&circuit_index);
+    return graph_longest_path(sccg);
 }
 
 
@@ -251,6 +289,6 @@ int main(int argc, char **argv) {
     printf("result = %d, expected = %d\n",
         getMaxVisitableWebpages(10, 9, (int []){3,2,5,9,10,3,3,9,4}, (int []){9,5,7,8,6,4,5,3,9}), 5);
     printf("result = %d, expected = %d\n",
-        getMaxVisitableWebpages(5, 6, (int []){1,2,3,3,4,5}, (int []){2,3,1,4,5,1}), 5);
+        getMaxVisitableWebpages(5, 6, (int []){1,2,3,3,4,5}, (int []){2,3,1,4,5,2}), 5);
 }
 
